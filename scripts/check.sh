4880b95434651c97863510bcc0e82d70c059b35a
ghurley <ghurley@murex.com>
2016-06-01 15:27:55 +0100

    Repair failing job

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Determine if the following files changed in the branch
-echo "Checking for modifications to files that should not be changed. (Blank if no changes foound)"
+echo "Checking for modifications to files that should not be changed. (Blank if no changes found)"
 git diff --exit-code `git rev-list HEAD | tail -n 2 | head -n 1` HEAD LICENSE README.md
