a364f6d7d57cdca6a0264d9f9e32626deb7e74be
ghurley <ghurley@murex.com>
2016-06-01 15:16:52 +0100

    Restore README.md and modify check.sh log

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Newcomer Testing of Github and Travis integration
 
 [![Build Status](https://travis-ci.org/garethhurley/github-testing.svg?branch=master)](https://travis-ci.org/garethhurley/github-testing)
 
-
--- scripts/check.sh
+++ scripts/check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Determine if the following files changed in the branch
-echo "Checking for modifications to files that should not be changed"
-git diff --exit-code --name-only `git rev-list HEAD | tail -n 2 | head -n 1` HEAD LICENSE README.md
+echo "Checking for modifications to files that should not be changed. (Blank if no changes foound)"
+git diff --exit-code `git rev-list HEAD | tail -n 2 | head -n 1` HEAD LICENSE README.md
